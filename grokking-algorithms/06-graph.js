033a143b62b6b20cd4f3ae0228bc592b80fe0fd8
vigdorov <vigdorov@yandex-team.ru>
2020-04-26 22:31:44 +0300

    grp

---
+++ grokking-algorithms/06-graph.js
@@ -0,0 +1,15 @@
+const graph = {
+    A: ['B', 'C'],
+    B: ['D', 'F'],
+    C: ['D', 'G'],
+    D: [],
+    F: [],
+    G: ['F'],
+};
+
+let queue = [];
+let steps = 0;
+
+const minPath = (startPoint, endPoint) => {
+    
+}
